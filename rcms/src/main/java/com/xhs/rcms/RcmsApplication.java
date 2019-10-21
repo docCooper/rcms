8d1394c0a1786351136a8e75eb16ada8d03e1aa7
曹通 <1361017281@qq.com>
2019-10-21 11:16:49 +0800

    初次创建项目

---
+++ rcms/src/main/java/com/xhs/rcms/RcmsApplication.java
@@ -0,0 +1,13 @@
+package com.xhs.rcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcmsApplication.class, args);
+	}
+
+}
---
+++ rcms/src/test/java/com/xhs/rcms/RcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhs.rcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
